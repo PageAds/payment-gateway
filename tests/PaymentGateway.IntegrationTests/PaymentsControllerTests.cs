2ceda965b4d46db4c1e1a2bccb01c4d42e4780f1
PageAds <adampage6@gmail.com>
2023-01-23 23:05:06 +0000

    TDD - Updating test

--- tests/PaymentGateway.IntegrationTests/PaymentsControllerTests.cs
+++ tests/PaymentGateway.IntegrationTests/PaymentsControllerTests.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using PaymentGateway.Models;
 using Shouldly;
 using System.Net;
+using System.Text;
 using Xunit;
 
 namespace PaymentGateway.IntegrationTests
@@ -19,7 +20,7 @@ namespace PaymentGateway.IntegrationTests
 
             var payment = new PaymentRequest("5479630754337041", 4, 2027, 10.00m, "GBP", "123");
 
-            var stringContent = new StringContent(JsonConvert.SerializeObject(payment));
+            var stringContent = new StringContent(JsonConvert.SerializeObject(payment), Encoding.UTF8, "application/json");
 
             // Act
             var response = await client.PostAsync("/payments", stringContent);
