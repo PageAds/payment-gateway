be798003fef95cde24ad67a98af9cda5416f512a
PageAds <adampage6@gmail.com>
2023-01-23 23:36:21 +0000

    TDD - Introduce integration test: POST /payments for returning the created payment in the http response body

---
+++ src/PaymentGateway/Models/PaymentResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace PaymentGateway.Models
+{
+    public class PaymentResponse : PaymentRequest
+    {
+        public PaymentResponse(string cardNumber, int cardExpiryMonth, int cardExpiryYear, decimal amount, string currency, string cvv, long id)
+            : base(cardNumber, cardExpiryMonth, cardExpiryYear, amount, currency, cvv)
+        {
+            Id = id;
+        }
+
+        public long Id { get; }
+    }
+}
--- tests/PaymentGateway.IntegrationTests/PaymentsControllerTests.cs
+++ tests/PaymentGateway.IntegrationTests/PaymentsControllerTests.cs
@@ -29,5 +29,43 @@ namespace PaymentGateway.IntegrationTests
             response.ShouldNotBeNull();
             response.StatusCode.ShouldBe(HttpStatusCode.Created);
         }
+
+        [Fact]
+        public async Task Post_WhenAPaymentIsCreated_ReturnsCreatedPaymentInHttpResponseBody()
+        {
+            // Arrange
+            var application = new WebApplicationFactory<Program>();
+
+            var client = application.CreateClient();
+
+            var cardNumber = "5479630754337041";
+            var cardExpiryMonth = 4;
+            var cardExpiryYear = 2027;
+            var amount = 10.00m;
+            var currency = "GBP";
+            var cvv = "123";
+
+            var paymentRequest = new PaymentRequest(cardNumber, cardExpiryMonth, cardExpiryYear, amount, currency, cvv);
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(paymentRequest), Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await client.PostAsync("/payments", stringContent);
+
+            // Assrt
+            response.ShouldNotBeNull();
+            response.Content.ShouldNotBeNull();
+            var responseContentString = await response.Content.ReadAsStringAsync();
+
+            var paymentResponse = JsonConvert.DeserializeObject<PaymentResponse>(responseContentString);
+            paymentResponse.ShouldNotBeNull();
+            paymentResponse.Id.ShouldBeGreaterThan(0);
+            paymentResponse.CardNumber.ShouldBe(cardNumber);
+            paymentResponse.CardExpiryMonth.ShouldBe(cardExpiryMonth);
+            paymentResponse.CardExpiryYear.ShouldBe(cardExpiryYear);
+            paymentResponse.Amount.ShouldBe(amount);
+            paymentResponse.Currency.ShouldBe(currency);
+            paymentResponse.CVV.ShouldBe(cvv);
+        }
     }
 }
