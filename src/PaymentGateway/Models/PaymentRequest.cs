f463c38cde6aac063f1ef9c607f555b124c1a268
PageAds <adampage6@gmail.com>
2023-01-23 22:56:06 +0000

    TDD - Introduce POST /payments integration test

---
+++ src/PaymentGateway/Models/PaymentRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace PaymentGateway.Models
+{
+    public class PaymentRequest
+    {
+        public PaymentRequest(string cardNumber, int cardExpiryMonth, int cardExpiryYear, decimal amount, string currency, string cvv)
+        {
+            CardNumber = cardNumber;
+            CardExpiryMonth = cardExpiryMonth;
+            CardExpiryYear = cardExpiryYear;
+            Amount = amount;
+            Currency = currency;
+            CVV = cvv;
+        }
+
+        public string CardNumber { get; }
+
+        public int CardExpiryMonth { get; }
+
+        public int CardExpiryYear { get; }
+
+        public decimal Amount { get; }
+
+        public string Currency { get; }
+
+        public string CVV { get; }
+    }
+}
---
+++ tests/PaymentGateway.IntegrationTests/PaymentsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using PaymentGateway.Models;
+using Shouldly;
+using System.Net;
+using Xunit;
+
+namespace PaymentGateway.IntegrationTests
+{
+    public class PaymentsControllerTests
+    {
+        [Fact]
+        public async Task Post_WhenAPaymentIsCreated_ReturnsHttpCreatedResponse()
+        {
+            // Arrange
+            var application = new WebApplicationFactory<Program>();
+
+            var client = application.CreateClient();
+
+            var payment = new PaymentRequest("5479630754337041", 4, 2027, 10.00m, "GBP", "123");
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(payment));
+
+            // Act
+            var response = await client.PostAsync("/payments", stringContent);
+
+            // Assrt
+            response.ShouldNotBeNull();
+            response.StatusCode.ShouldBe(HttpStatusCode.Created);
+        }
+    }
+}
