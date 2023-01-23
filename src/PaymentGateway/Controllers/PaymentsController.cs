bdf3bb10893b7a2a345d172fa225d6178e625d72
PageAds <adampage6@gmail.com>
2023-01-23 23:24:35 +0000

    TDD - Test passed: POST /payments integration

---
+++ src/PaymentGateway/Controllers/PaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentGateway.Models;
+
+namespace PaymentGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentsController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(PaymentRequest paymentRequest)
+        {
+            return CreatedAtAction(nameof(Create), null);
+        }
+    }
+}
