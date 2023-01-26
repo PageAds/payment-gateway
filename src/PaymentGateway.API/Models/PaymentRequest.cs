0c6cab150804855b3620f6316e2d475fb5055514
PageAds <adampage6@gmail.com>
2023-01-26 09:37:02 +0000

    Add property descriptions

--- src/PaymentGateway.API/Models/PaymentRequest.cs
+++ src/PaymentGateway.API/Models/PaymentRequest.cs
@@ -12,16 +12,34 @@
             CVV = cvv;
         }
 
+        /// <summary>
+        /// Primary account number
+        /// </summary>
         public string CardNumber { get; }
 
+        /// <summary>
+        /// The month the card expires in numerical format (1 through 12)
+        /// </summary>
         public int CardExpiryMonth { get; }
 
+        /// <summary>
+        /// The year the card expires (yyyy)
+        /// </summary>
         public int CardExpiryYear { get; }
 
+        /// <summary>
+        /// Payment amount
+        /// </summary>
         public decimal Amount { get; }
 
+        /// <summary>
+        /// Currency of the payment in ISO 4217 format (e.g. "GBP" or "USD")
+        /// </summary>
         public string Currency { get; }
 
+        /// <summary>
+        /// Card verification value
+        /// </summary>
         public string CVV { get; }
     }
 }
--- src/PaymentGateway.API/Models/ViewModels/Payment.cs
+++ src/PaymentGateway.API/Models/ViewModels/Payment.cs
@@ -17,20 +17,44 @@
             Currency = currency;
         }
 
+        /// <summary>
+        /// Identifier of the payment
+        /// </summary>
         public long Id { get; }
 
+        /// <summary>
+        /// Status of the payment (either "Pending" or "Completed")
+        /// </summary>
         public string Status { get; }
 
+        /// <summary>
+        /// Primary account number
+        /// </summary>
         public string CardNumber { get; }
 
+        /// <summary>
+        /// The month the card expires in numerical format (1 through 12)
+        /// </summary>
         public int CardExpiryMonth { get; }
 
+        /// <summary>
+        /// The year the card expires (yyyy)
+        /// </summary>
         public int CardExpiryYear { get; }
 
+        /// <summary>
+        /// Payment amount
+        /// </summary>
         public decimal Amount { get; }
 
+        /// <summary>
+        /// Currency of the payment in ISO 4217 format (e.g. "GBP")
+        /// </summary>
         public string Currency { get; }
 
+        /// <summary>
+        /// Card verification value
+        /// </summary>
         public string CVV { get; }
     }
 }
